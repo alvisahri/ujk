426c36d6c3d872210e9a3ac57d5e9cf97bc390fe
Ais Wallenstein <83851691+orion2182@users.noreply.github.com>
2025-06-13 20:17:36 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,151 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" #contoh
+    }
+  }
+}
+
+provider "aws" {
+  region = ""
+}
+
+resource "" "" {
+  cidr_block = ""
+  tags = {
+    Name = ""
+  }
+}
+
+resource "" {
+  count                   = 
+  vpc_id                  = 
+  cidr_block              = 
+  availability_zone       = 
+  map_public_ip_on_launch = 
+  tags = {
+    Name = ""
+  }
+}
+
+resource "" {
+  count             = 
+  vpc_id            = 
+  cidr_block        = ""
+  availability_zone = ""
+  tags = {
+    Name = ""
+  }
+}
+
+resource "" {
+  vpc_id = 
+  tags = {
+    Name = ""
+  }
+}
+
+resource "" {
+  vpc_id = 
+  route {
+    cidr_block = ""
+    gateway_id = 
+  }
+  tags = {
+    Name = ""
+  }
+}
+
+resource "" {
+  count          = 
+  subnet_id      = 
+  route_table_id = 
+}
+
+resource "" {
+  domain = ""
+}
+
+resource "" "" {
+  allocation_id = 
+  subnet_id     = 
+  tags = {
+    Name = ""
+  }
+  depends_on = []
+}
+
+resource "" "" {
+  vpc_id = 
+  route {
+    cidr_block     = ""
+    nat_gateway_id = 
+  }
+  tags = {
+    Name = ""
+  }
+}
+
+resource "" {
+  count          = 
+  subnet_id      = 
+  route_table_id = 
+}
+
+resource "" {
+  name        = ""
+  description = ""
+  vpc_id      = 
+
+  ingress {
+    description = ""
+    from_port   = 
+    to_port     = 
+    protocol    = ""
+    cidr_blocks = [""]
+  }
+
+  ingress {
+    description = ""
+    from_port   = 
+    to_port     = 
+    protocol    = ""
+    cidr_blocks = [""]
+  }
+
+  ingress {
+    description = ""
+    from_port   = 
+    to_port     = 
+    protocol    = ""
+    cidr_blocks = [""]
+  }
+
+  egress {
+    from_port   = 
+    to_port     = 
+    protocol    = ""
+    cidr_blocks = [""]
+  }
+}
+
+resource "" {
+  ami           = ""
+  instance_type = ""
+  subnet_id     = 
+  security_groups = 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y nginx
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              echo "<h1>UJK BNSP <ISI NAMA ANDA></h1>" | sudo tee /var/www/html/index.html #HAPUS TANDA <>
+              EOF
+
+  tags = {
+    Name = ""
+  }
+}
