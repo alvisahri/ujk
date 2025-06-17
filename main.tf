d191d0a53c5fd299654ba7d38c8c7196304f19d1
Ais Wallenstein <83851691+orion2182@users.noreply.github.com>
2025-06-17 13:49:12 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,11 @@ terraform {
 }
 
 provider "aws" {
-  region     = "ap-southeast-1"
-  access_key = ""         
-  secret_key = "" 
+  region = "ap-southeast-1"
+    access_key = "" 
+    secret_key = "" 
 }
+
 
 resource "aws_vpc" "main" {
   cidr_block = "10.20.0.0/16"
@@ -21,15 +22,26 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.20.${count.index + 1}.0/24"
-  availability_zone       = "ap-southeast-1${element(["a", "b"], count.index)}"
-  map_public_ip_on_launch = true
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.20.${count.index + 1}.0/24" 
+  availability_zone = "ap-southeast-1${element(["a", "b"], count.index)}" 
+  map_public_ip_on_launch = true 
   tags = {
     Name = "aetherlock2-public-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.20.10${count.index + 1}.0/24" 
+  availability_zone = "ap-southeast-1${element(["a", "b"], count.index)}"
+  tags = {
+    Name = "aetherlock2-private-subnet-${count.index + 1}"
+  }
+}
+
 
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main.id
@@ -55,30 +67,27 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Security Group
 resource "aws_security_group" "web_sg" {
-  name        = "aetherlock2-sg"
+  name        = "-sg"
   description = "Allow HTTP, HTTPS, and SSH access"
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "HTTP"
+    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
-    description = "HTTPS"
+    description = "Allow HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
-    description = "SSH"
+    description = "Allow SSH from anywhere (untuk latihan)"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -88,17 +97,17 @@ resource "aws_security_group" "web_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1" 
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "aetherlock2_server" {
-  ami           = "ami-0b8607d2721c94a77" 
-  instance_type = "t2.micro"             
+  ami           = "ami-0b8607d2721c94a77"
+  instance_type = "t2.micro"
   subnet_id     = aws_subnet.public[0].id
   security_groups = [aws_security_group.web_sg.id]
-  key_name        = "ujk"                 
+  key_name        = ""
 
   user_data = <<-EOF
               #!/bin/bash
@@ -108,8 +117,10 @@ resource "aws_instance" "aetherlock2_server" {
               sudo systemctl enable nginx
               echo "<h1>UJK BNSP <nama kalian></h1>" | sudo tee /var/www/html/index.html
               EOF
-
+              
   tags = {
     Name = "WebServer-Test"
   }
 }
+
+
